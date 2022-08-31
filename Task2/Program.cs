97542e37d920c842fd9706bd5a890d829f64f85f
Aleksey <del-fisha@yandex.ru>
2022-08-31 12:03:25 +0300

    Решение

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -4,19 +4,19 @@ int num1 = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите число 2:");
 int num2 = Convert.ToInt32(Console.ReadLine());
 
-if(num1 > num2)
+if (num1 > num2)
 {
     Console.Write($"Максимальное число: {num1}");
     Console.Write($" Минимальное число: {num2}");
 }
 
-if(num1 < num2)
+if (num1 < num2)
 {
     Console.Write($"Максимальное число: {num2}");
     Console.Write($" Минимальное число: {num1}");
 }
 
-if(num1 == num2)
+if (num1 == num2)
 {
     Console.WriteLine("Числа равны");
 }
