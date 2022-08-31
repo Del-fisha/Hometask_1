39c3d7a348d38433a18a90d0103eafbbff720e9c
Aleksey <del-fisha@yandex.ru>
2022-08-31 12:02:14 +0300

    Решение

--- Task4/Program.cs
+++ Task4/Program.cs
@@ -7,8 +7,8 @@ Console.Write("Введите число 3: ");
 int num3 = Convert.ToInt32(Console.ReadLine());
 int max = num1;
 
-    if(num2 > max) max = num2;
-    if(num3 > max) max = num3;
+if (num2 > max) max = num2;
+if (num3 > max) max = num3;
 
 Console.Write($"Минимальное число: {max}");
 
